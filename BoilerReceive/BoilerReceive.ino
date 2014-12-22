4067d5226143a6d7bbb536dc3da680e7a106b50a
Suhwan Hwang <entercritical@gmail.com>
2014-12-23 00:03:16 +0900

    add toggle button (PIN 13)

--- BoilerReceive/BoilerReceive.ino
+++ BoilerReceive/BoilerReceive.ino
@@ -7,12 +7,13 @@
 #include <RCSwitch.h>
 
 const int PIN_SWITCH = 4;    // Relay Switch
+const int PIN_TOGGLE_BUTTON = 13;
 const int BOILER_ON = 1001;  // Boiler ON code
 const int BOILER_OFF = 1000; // Boiler OFF code  
 
 RCSwitch mySwitch = RCSwitch();
 boolean isBoilerOn = false;
-
+int prevButtonState = 1;
 
 void setBoilerOn(boolean on) {
   if (on == isBoilerOn) {
@@ -34,20 +35,22 @@ void setBoilerOn(boolean on) {
 void setup() {
   Serial.begin(9600);
   
-  #ifdef __AVR_ATmega32U4__ // for Leonardo, Micro
-  while (!Serial) ;
-  Serial.println("Ready!");
-  #endif 
-  
   // Receiver on interrupt 0 => that is pin #2 (Uno)
   // Leonardo, Micro pin #3
   mySwitch.enableReceive(0);  
   
   pinMode(PIN_SWITCH, OUTPUT);
   digitalWrite(PIN_SWITCH, LOW);
+  pinMode(PIN_TOGGLE_BUTTON, INPUT);
 }
 
 void loop() {
+  int buttonState = digitalRead(PIN_TOGGLE_BUTTON);
+  if (buttonState == 0 && prevButtonState != buttonState) {
+      setBoilerOn(!isBoilerOn);
+  }
+  prevButtonState = buttonState;
+  
   if (mySwitch.available()) {
     
     int value = mySwitch.getReceivedValue();
