1c4fabc77efdfa0b7f87aac8d88e02a25da2f0b1
Suhwan Hwang <entercritical@gmail.com>
2015-12-27 08:37:43 +0900

    mysql connection close bug

--- HomeServer/config/passport.js
+++ HomeServer/config/passport.js
@@ -20,16 +20,30 @@ var validPassword = function (password, crypted) {
 // load up the user model
 //var User       		= require('../models/user');
 var mysql = require('mysql');
-var connection = mysql.createConnection(mysql_info);
+var connection;
 
-connection.connect(function (err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
-});
+function handleDisconnect() {
+    connection = mysql.createConnection(mysql_info); // Recreate the connection, since
+                                                     // the old one cannot be reused.
 
+    connection.connect(function (err) {              // The server is either down
+        if (err) {                                   // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        }                                       // to avoid a hot loop, and to allow our node script to
+        console.log('mysql connected as id ' + connection.threadId);// process asynchronous requests in the meantime.
+    });                                            // If you're also serving http, display a 503 error.
+    connection.on('error', function (err) {
+        console.log('db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                        // lost due to either server restart, or a
+        } else {                                       // connnection idle timeout (the wait_timeout
+            throw err;                                 // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
 
 // expose this function to our app using module.exports
 module.exports = function (passport) {
